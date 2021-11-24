8f373689ac25a3971de829a80ca1335693b77f30
JeonHaNeul <93646786+zqxw116@users.noreply.github.com>
2021-11-24 14:02:00 +0900

    Fire Commit
    Fire Commit 제작

---
+++ 01Projext/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 총알을 생산할 공장
+    public GameObject bulletFactory;
+    // 총구
+    public GameObject firePosition;
+
+    private void Update()
+    {
+        // 목표: 사용자가 발사 버튼을 누르면 총알을 발사하고 싶다.
+        // 순서: 1. 사용자가 발사 버튼을 누르면
+        // 만약 사용자가 발사 버튼을 누르면
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // 2. 총알 공장에서 총알을 만든다
+            // 3. 총알을 발사한다
+        }
+    }
+}
---
+++ 01Projext/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    // 플레이어가 이동할 속력
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, v, 0);
+
+        // P(미래의 위치) = P0(현재의 위치) + vt(속도x시간)
+        // transform.position = transform.position + dir * speed * Time.deltaTime;
+        transform.position += dir * speed * Time.deltaTime;
+
+    }
+}
